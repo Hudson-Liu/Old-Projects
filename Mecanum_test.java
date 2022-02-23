6b6840fe5a1d4e64079f3d6d9ac8e7a99a502bb8
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-02-22 19:47:03 -0500

    Fixed all code
    Now there are 2 variations, one for autonomous that uses theta and an overall power, and one for teleop that uses x and y components

--- Mecanum_test.java
+++ Mecanum_test.java
@@ -1,41 +1,66 @@
-package euueuee;
+package ueueeu;
+
 import java.util.Scanner;
-import java.lang.Math;
+import java.util.*;
 
-public class ueueueueue {
-
+public class aef {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Speed: ");
-	    int speed = sc.nextInt();
-	    System.out.print("Theta: ");
-	    int theta = sc.nextInt();
-	    double rad = Math.toRadians(theta);
-	    double V_h = speed*(Math.sin(rad));
-	    double V_v = speed*(Math.cos(rad));
-	    double w_1 = 0.25*(V_v-V_h)*Math.sqrt(2);
-	    double w_2 = 0.25*(V_v-V_h)*Math.sqrt(2);
-	    double w_3 = 0.25*(V_h+V_v)*Math.sqrt(2);
-	    double w_4 = 0.25*(V_h+V_v)*Math.sqrt(2);
-	    System.out.println(String.valueOf(w_1) + " " + String.valueOf(w_2) + " " + String.valueOf(w_3) + " " + String.valueOf(w_4));
-	    System.out.println(Math.sqrt(Math.pow(w_1+w_2, 2) + Math.pow(w_3+w_4, 2)));
-	    */
 		
+		//Takes Speed, Theta, and Rotation as arguments, used for Autonomous as it's easier to fine-tune (supposedly)
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Speed: ");
 	    double speed = sc.nextDouble();
 	    System.out.print("Theta: ");
 	    int theta = sc.nextInt();
+	    System.out.print("Rotation: ");
+	    double r = sc.nextDouble();
+	    
 	    double rad = Math.toRadians(theta);
 	    double V_h = speed*Math.sin(rad);
 	    double V_v = speed*Math.cos(rad);
-	    double w_1 = 0.5*(V_v-V_h)*Math.sqrt(2);
-	    double w_2 = 0.5*(V_v-V_h)*Math.sqrt(2);
-	    double w_3 = 0.5*(V_h+V_v)*Math.sqrt(2);
-	    double w_4 = 0.5*(V_h+V_v)*Math.sqrt(2);
-	    System.out.println(String.valueOf(w_1) + " " + String.valueOf(w_2) + " " + String.valueOf(w_3) + " " + String.valueOf(w_4));
-	    System.out.println(Math.sqrt(Math.pow(w_1+w_2, 2) + Math.pow(w_3+w_4, 2)));
+	    double denominator = Math.max(Math.abs(V_v) + Math.abs(V_h) + Math.abs(r), 1);
+	    
+	    double t1 = (V_v-V_h+r)/denominator;//Back Left
+	    double t2 = (V_v-V_h-r)/denominator;//Front Right
+	    double t3 = (V_h+V_v-r)/denominator;//Back Right
+	    double t4 = (V_h+V_v+r)/denominator;//Front Left
+	    
+	    System.out.println(String.valueOf(t1) + " " + String.valueOf(t2) + " " + String.valueOf(t3) + " " + String.valueOf(t4));
+	    System.out.println(String.valueOf(t1+t2+t3+t4));
+		
+		/*
+		//Uses Horizontal Component and Vertical Components, will be used for Joystick control
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Horizontal Component: ");
+	    double V_h = sc.nextDouble();
+	    System.out.print("Vertical Component: ");
+	    double V_v = sc.nextDouble();
+	    System.out.print("Rotation :");
+	    double r = sc.nextDouble();
+	    double denominator = Math.max(Math.abs(V_v) + Math.abs(V_h) + Math.abs(r), 1);
+	    double t1 = (V_v-V_h+r)/denominator;//Back Left
+	    double t2 = (V_v-V_h-r)/denominator;//Front Right
+	    double t3 = (V_h+V_v-r)/denominator;//Back Right
+	    double t4 = (V_h+V_v+r)/denominator;//Front Left
+	    System.out.println(String.valueOf(t1) + " " + String.valueOf(t2) + " " + String.valueOf(t3) + " " + String.valueOf(t4));
+	    System.out.println(String.valueOf(t1+t2+t3+t4));
+	    */
+	    
+		/*
+		//A straight-up broken script that's good for nothing
+		Scanner sc = new Scanner(System.in);
+		System.out.print("X: ");
+	    double x = sc.nextDouble()*1.1;
+	    System.out.print("Y: ");
+	    double y = -1*sc.nextDouble();
+	    System.out.print("Rotate: ");
+	    double rx = sc.nextDouble();
+		double w_1 = y + x + rx;
+		double w_2 = y - x + rx;
+		double w_3 = y - x - rx;
+		double w_4 = y + x - rx;
+		System.out.println(String.valueOf(w_1) + " " + String.valueOf(w_2) + " " + String.valueOf(w_3) + " " + String.valueOf(w_4));
+		*/
 	}
 }
