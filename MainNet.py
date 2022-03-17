205c4ebc20da4735155125bffcf716bd7d30583b
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-03-17 17:10:24 -0400

    Half-Finished Model
    Ok *now* the model is half-finished, before it was like a quarter done at most. All that's left now is the genetic algorithm and finishing the training loop for the MainNet. Oh also there are soooo many issues with the MainNet right now.

--- MainNet.py
+++ MainNet.py
@@ -23,64 +23,85 @@ import glob
 
 
 class MainNet: #holds MainNet, Weight iterator, and Bias iterator
-    def __init__(epochsPerMainNet, epochsPerSubNet, SubNet):
-        self.epochsPerMainNet = epochsPerMainNet
-        self.epochsPerSubNet = epochsPerSubNet #the LSTM will take exactly as many timesteps as epochs of the SubNet
-        self.SubNet = SubNet
-        mainNetInit()
-        weightIteratorInit()
+    def __init__(self):
+        self.logitsInterpreterInit()
+        self.intentionNetInit()
+        self.weightIteratorInit()
+        self.hyperParameter_Optimizer_Init()
         #make a new function to import a new subnet function so that the actual training
         #loop just needs to hand off a new SubNet to the mainNetTrainingLoop function each epoch
         
     #updates every training step/epoch for the SubNet
-    def setSubNet(SubNet):
+    def setSubNet(self, SubNet):
         self.SubNet = SubNet
         
-    def mainNetInit(self):
-        #finds the output shape of the subnet
-        unprocessed_weights = self.SubNet.layers[len(self.SubNet.layers) - 1].get_weights() #get the weights and biases of the last layer of the subnet
-        num_of_biases = len(unprocessed_weights[1]) #get the number of biases
-        
-        inputs = keras.Input(batch_input_shape = (1, num_of_biases, 2)) #the shape is (one single logit per batch, total amount of logits from subnet, individual logit + correct logit)
+    def logitsIntepreterInit(self):
+        #creates stacked lstm, all these values i pulled out of my arse
+        inputs = keras.Input(shape = (None, 2)) #the shape is (one single logit per batch, total amount of logits from subnet (variable so we dont give it an input), individual logit + correct logit)
         LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
-        LSTMlayer2 = keras.layers.LSTM(1000, return_sequences=True)(LSTMlayer1)
-        LSTMlayer3 = keras.layers.LSTM(1000, return_sequences=True)(LSTMlayer2)
-        outputs = keras.layers.LSTM(1, return_sequences=True)(LSTMlayer3) #modified logit
-        jimmy = keras.Model(inputs=inputs, outputs=outputs, name="Jimmy")
-        jimmy.summary()
+        LSTMlayer2 = keras.layers.LSTM(500, return_sequences=False)(LSTMlayer1)
+        outputs = keras.layers.Dense(100, activation='softmax')(LSTMlayer2) #random encoded output
+        self.logits_interpreter = keras.Model(inputs=inputs, outputs=outputs, name="interpreter")
+        self.logits_interpreter.summary()
+    
+    #intention net is literally just the intention of the network dont ask
+    def intentionNetInit(self):
+        #find output size of logits_interpreter
+        unprocessed_weights = self.logits_interpreter.layers[len(self.logits_interpreter.layers) - 1].get_weights() #get the weights and biases of the last layer of the subnet
+        num_of_biases = len(unprocessed_weights[1]) #get the number of biases, which is equal to the amoutn of output nodes
+        
+        #the shape is (one single interpreter output per batch, number of epochs the subnet has to run, size of output of interpreter)
+        inputs = keras.Input(shape = (None, num_of_biases)) 
+        LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
+        LSTMlayer2 = keras.layers.LSTM(500, return_sequences=True)(LSTMlayer1)
+        outputs = keras.layers.Dense(10, activation='softmax')(LSTMlayer2) #random encoded output
+        self.intention = keras.Model(inputs=inputs, outputs=outputs, name="intention")
+        self.intention.summary()
         
     def weightIteratorInit(self):
-        
-        #find how many weights there are for the Weight Iterator
-        numberOfWeights = 0
-        for layer in self.SubNet.layers:
-            numberOfWeights += len(layer.get_weights())
-        inputs = keras.Input(shape = (numberOfWeights, 5))
+        #the shape is (one single intention per batch, number of weights in SubNet (cant be specified since when running this same model across many other SubNets, they wont be identically sized), individual weight + intention)
+        #output size of intentionNet
+        unprocessed_weights = self.logits_interpreter.layers[len(self.logits_interpreter.layers) - 1].get_weights() #get the weights and biases of the last layer of the subnet
+        num_of_biases = len(unprocessed_weights[1]) #get the number of biases, which is equal to the amoutn of output nodes
         
         #initializaes model
+        inputs = keras.Input(shape = (None, (1 + num_of_biases)))
         LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
-        LSTMlayer2 = keras.layers.LSTM(1000, return_sequences=True)(LSTMlayer1)
-        LSTMlayer3 = keras.layers.LSTM(1000, return_sequences=True)(LSTMlayer2)
-        outputs = keras.layers.LSTM(1000, return_sequences=True)(LSTMlayer3)
-        weights = keras.Model(inputs=inputs, outputs=outputs, name="weights")
-        weights.summary()
-        
+        LSTMlayer2 = keras.layers.LSTM(500, return_sequences=True)(LSTMlayer1)
+        outputs = keras.layers.TimeDistributed(keras.layers.Dense(1, activation='softmax'))(LSTMlayer2)
+        self.weights = keras.Model(inputs=inputs, outputs=outputs, name="weights")
+        self.weights.summary()
+        #finish weight iterator, make bias iterator, finish training step
+    
+    def hyperParameter_Optimizer_Init(self):
+        inputs = keras.Input(shape=(3,)) #num_features, num_depVar, num_samples
+        FFNNlayer1 = keras.layers.Dense(100, activation='relu')(inputs)
+        FFNNlayer2 = keras.layers.Dense(100, activation='relu')(FFNNlayer1)
+        outputs = keras.layers.Dense(4, activation='sigmoid')(FFNNlayer2)
+        self.hyperparameters = keras.Model(inputs=inputs, outputs=outputs, name="hyperparameters")
+        self.hyperparameters.summary()
+    """
     def setWeights(self):
         for(how many layers there are):
-        layer.setWeights()v
+            layer.setWeights()
     
-    #a single step of training
-    def mainNetTrainingStep():
+    #a single step of training, logits are raw logits from SubNet
+    def mainNetTrainingStep(SubNet_logits, correct_logits):
         
+        logits_interpreter(logits, correct)
             
-            
+    
+        
     def genAlg(): #generational algorithm
-        
+        """
 class SubNet:
     #add required specification of output_size to metadata document
-    def __init__(layer_number, node_number, activation, dataset, output_size): #int, int, str, np array
-            subNet = keras.Input(shape = (node_number))
-            
+    def __init__(self, num_layers, activation, num_nodes, dataset, num_features, output_size): #int,
+            self.subNet = keras.Sequential()
+            self.subNet.add(keras.layers.Input(shape = (num_features,)))
+            nodes_per_layer = num_nodes/num_layers
+            for i in range(num_layers):
+                self.subNet.add(keras.layers.Dense(nodes_per_layer))
 
 class MainClass:
     def __init__(self, existing_preprocessed_data, import_sequentially): #both are boolean
@@ -124,7 +145,7 @@ class MainClass:
         cwd = os.getcwd()
         for file in files:
             file = cwd + "\\Metadata\\" + file
-            Data_PreProcessor.PreProcessor(cwd, file, counter)
+            len_features, len_depVar = Data_PreProcessor.PreProcessor(cwd, file, counter)
             counter += 1
     
     def importAllData(self, datafiles):
@@ -136,15 +157,18 @@ class MainClass:
             datasets.append(np.load(filepath)) 
         return datasets
             
-    def seqImportData(number):
+    def seqImportData(datafiles, number):
         cwd = os.getcwd()
         filepath = repr(datafiles[number])[1:-1]
         filepath = cwd + "\\PreProcessed_Data\\" + filepath
         dataset = np.load(filepath)
         
         return dataset
-    
+
+    #training loop for single solution
     def training_loop():
+        mainNet = MainNet()
+        mainNet.hyperparameters()
         
             
-jimmy = MainNet(50) #the only parameter is how many epochs each SubNet is ran
+#jimmy = MainNet(50) #the only parameter is how many epochs each SubNet is ran
