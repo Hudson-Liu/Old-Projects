e685b36805ba68ef270b4653ba9fc7184657f885
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-03-19 09:43:25 -0400

    Update Jimmy.py

--- Jimmy_MK_II/Jimmy.py
+++ Jimmy_MK_II/Jimmy.py
@@ -20,7 +20,8 @@ from tensorflow import keras
 import Data_PreProcessor
 import os
 import glob
-
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
 
 class MainNet: #holds MainNet, Weight iterator, and Bias iterator
     def __init__(self):
@@ -35,7 +36,7 @@ class MainNet: #holds MainNet, Weight iterator, and Bias iterator
     def setSubNet(self, SubNet):
         self.SubNet = SubNet
         
-    def logitsIntepreterInit(self):
+    def logitsInterpreterInit(self):
         #creates stacked lstm, all these values i pulled out of my arse
         inputs = keras.Input(shape = (None, 2)) #the shape is (one single logit per batch, total amount of logits from subnet (variable so we dont give it an input), individual logit + correct logit)
         LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
@@ -51,7 +52,7 @@ class MainNet: #holds MainNet, Weight iterator, and Bias iterator
         num_of_biases = len(unprocessed_weights[1]) #get the number of biases, which is equal to the amoutn of output nodes
         
         #the shape is (one single interpreter output per batch, number of epochs the subnet has to run, size of output of interpreter)
-        inputs = keras.Input(shape = (None, num_of_biases)) 
+        inputs = keras.Input(shape = (None, num_of_biases)) #num of biases stays constant, im just doing this for the sake of encapsulation
         LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
         LSTMlayer2 = keras.layers.LSTM(500, return_sequences=True)(LSTMlayer1)
         outputs = keras.layers.Dense(10, activation='softmax')(LSTMlayer2) #random encoded output
@@ -65,7 +66,7 @@ class MainNet: #holds MainNet, Weight iterator, and Bias iterator
         num_of_biases = len(unprocessed_weights[1]) #get the number of biases, which is equal to the amoutn of output nodes
         
         #initializaes model
-        inputs = keras.Input(shape = (None, (1 + num_of_biases)))
+        inputs = keras.Input(shape = (None, (1 + num_of_biases))) #the one is the actual weight
         LSTMlayer1 = keras.layers.LSTM(1000, return_sequences=True)(inputs)
         LSTMlayer2 = keras.layers.LSTM(500, return_sequences=True)(LSTMlayer1)
         outputs = keras.layers.TimeDistributed(keras.layers.Dense(1, activation='softmax'))(LSTMlayer2)
@@ -102,11 +103,13 @@ class SubNet:
             nodes_per_layer = num_nodes/num_layers
             for i in range(num_layers):
                 self.subNet.add(keras.layers.Dense(nodes_per_layer))
+            self.subNet.add(keras.layers.Dense(output_size))
 
 class MainClass:
-    def __init__(self, existing_preprocessed_data, import_sequentially): #both are boolean
+    def __init__(self, existing_preprocessed_data, import_sequentially, epochs_per_mainNet): #both are boolean
         self.existing_preprocessed_data = existing_preprocessed_data
         self.import_sequentially = import_sequentially
+        self.epochs_per_mainNet = epochs_per_mainNet
         
         #preprocess data if necessary
         if not self.existing_preprocessed_data:
@@ -122,9 +125,6 @@ class MainClass:
         
         if not self.import_sequentially:
             self.datasets = self.importAllData(datafiles)
-        
-        #open info necessary for hyperparameter optimizer
-        open()UJ08usda098f
     
     #create a list of the files that are going to either be opened or preprocessed
     def findFilepaths(self, path, extension):
@@ -159,7 +159,7 @@ class MainClass:
             datasets.append(np.load(filepath)) 
         return datasets
             
-    def seqImportData(datafiles, number):
+    def seqImportData(self, datafiles, number):
         cwd = os.getcwd()
         filepath = repr(datafiles[number])[1:-1]
         filepath = cwd + "\\PreProcessed_Data\\" + filepath
@@ -168,10 +168,20 @@ class MainClass:
         return dataset
     
     #training loop for single solution
-    def training_loop():
-        mainNet = MainNet()
-        
-        mainNet.hyperparameters()
+    def training_loop(self):
+        for i in range(self.epochs_per_mainNet):
+            
+            #initializing the subnet via the hyperparameter optimizer
+            mainNet = MainNet()
+            file = open("Dataset_Info.txt", "r", encoding='utf-8-sig')
+            lines = file.readlines()
+            dataset_info = (lines[i].replace("\n", "")).split(" ")
+            scaler = StandardScaler()
+            dataset_info = scaler.fit_transform(dataset_info)
+            tensorflow_dataset_info =  tf.data.Dataset.from_tensor_slices(dataset_info)
+            hyperparameters = mainNet.hyperparameters(tensorflow_dataset_info) #forward propagation of hyperparameter optimizer
+            subNetInstance = SubNet(hyperparameters)
+            
         
             
 #jimmy = MainNet(50) #the only parameter is how many epochs each SubNet is ran
