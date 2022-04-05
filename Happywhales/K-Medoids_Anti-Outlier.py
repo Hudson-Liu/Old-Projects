e5a9ced13c49ae2a230fe979a103b9cd112fe346
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-04-04 21:28:22 -0400

    Made it resistant to any input
    i added 2000 if statements so that, no matter the input, it wont return an error

--- Happywhales/K-Medoids_Anti-Outlier.py
+++ Happywhales/K-Medoids_Anti-Outlier.py
@@ -18,25 +18,41 @@ import matplotlib.pylab as pl
 
 class Modified_KMedoids:
     #returns trained kmedoids model
-    def k_medoids(self, array, num_clusters, iterations, percentile):
+    @staticmethod
+    def k_medoids(array, num_clusters, iterations, percentile):
         #k medoids clustering
         removed = []
         history = []
         removed = np.empty((0,0), float)
+        dimensions = len(array[0])
+        
         for i in range(0, iterations + 1):
             #train on data w/o outliers
-            medoids = KMedoids(n_clusters = clusters, max_iter = 300).fit(array)
-            centroids = medoids.cluster_centers_
+            if (np.size(array, axis=0) != 0 and num_clusters < np.size(array, axis=0)):
+                medoids = KMedoids(n_clusters = clusters, max_iter = 300).fit(array)
+                centroids = medoids.cluster_centers_
+            elif np.size(array, axis=0 == 0):
+                error_message = "All datapoints have been identified as outliers in iteration {iteration}. The percentile is likely set too low."
+                print(error_message.format(iteration = i))
+                break
+            else:
+                error_message = "The number of datapoints is less than the number of clusters in iteration {iteration}"
+                print(error_message.format(iteration = i))
+                break
             
             #add back the removed points
             if (i != 0): #janky fix but it works
-                history.append([array, centroids, removed, medoids])
-                array = np.append(array, removed, axis = 0)
+                if (np.size(removed, axis=0) != 0):
+                    history.append([array, centroids, removed, medoids])
+                    array = np.append(array, removed, axis = 0)
+                else:
+                    error_message = "No outliers have been detected in iteration {iteration}. The percentile is likely set too high."
+                    print(error_message.format(iteration = i))
+                    break
             predictions = medoids.predict(array)
             
             #find distance from each datapoint to centroid
             distances = []
-            dimensions = len(array[0])
             for l in range(0, len(array)): #for each datapoint
                 respective_centroid = centroids[predictions[l]] #find the corresponding centroid for the given datapoint
                 distance = 0
@@ -56,28 +72,32 @@ class Modified_KMedoids:
                 array = np.delete(array, remove[e], axis=0)
             removed = np.array(removed)
     
-        if dimensions == 2 or dimensions == 3:
+        if dimensions in (2, 3): #if it's 2 or 3 dimensional
             images = []
             customFont = ImageFont.truetype("arial.ttf", 24)
             for i in range(iterations):
-                if dimensions == 2:
-                    fig = self.two_dim_vis(history[i][0], history[i][1], history[i][2], history[i][3], num_clusters)
+                if dimensions == 2 and np.size(history, axis=0) > 0:
+                    fig = Modified_KMedoids.two_dim_vis(history[i][0], history[i][1], history[i][2], history[i][3], num_clusters)
+                elif dimensions == 3 and np.size(history, axis=0) > 0:
+                    fig = Modified_KMedoids.three_dim_vis(history[i][0], history[i][1], history[i][2], history[i][3], num_clusters)
                 else:
-                    fig = self.three_dim_vis(history[i][0], history[i][1], history[i][2], history[i][3], num_clusters)
+                    print("The program failed on it's first iteration, so there is no data to display.")
+                    break
                 fig.canvas.draw() #Depending on the backend, the canvas is not automatically drawn, and hence needs to first be manually drawn
                 image = PIL.Image.frombytes('RGB', fig.canvas.get_width_height(), fig.canvas.tostring_rgb())
                 manipulable_image = ImageDraw.Draw(image)
                 manipulable_image.text((10, 10), "Iteration Number: " + str(i + 1), font = customFont, fill=(0, 0, 0))
                 images.append(image)
-                
-            first_img = images[0]
-            first_img.save(fp='clustering.gif', format='GIF', append_images=images, save_all=True, duration=300, loop=0)
-            #ani.save('animation.gif', writer='pillow')
-            
+            if (len(images) > 0):   
+                first_img = images[0]
+                first_img.save(fp='clustering.gif', format='GIF', append_images=images, save_all=True, duration=300, loop=0)
+            else:
+                print("No images were able to be created.")
         return medoids
     
     #The name is short for "Two Dimensional Visualization"; visualizes 2d clusters using MatPlotLib
-    def two_dim_vis(self, array, centroids, removed, medoids, num_clusters):
+    @staticmethod
+    def two_dim_vis(array, centroids, removed, medoids, num_clusters):
         fig = plt.figure(figsize=(7, 7))
         ax = plt.axes(xlim=(0,1),ylim=(0,1))
         ax.set_title("K-Medoids Algorithm Results")
@@ -111,7 +131,8 @@ class Modified_KMedoids:
         plt.show()
         return fig
     
-    def three_dim_vis(self, array, centroids, removed, medoids, num_clusters):
+    @staticmethod #NOTE TO SELF: Any function that doesn't use self can be a staticmethod, since it's independent of the instance of the class
+    def three_dim_vis(array, centroids, removed, medoids, num_clusters):
         fig = plt.figure(figsize=(7, 7))
         ax = plt.axes(projection='3d')
         ax.set_title("K-Medoids Algorithm Results")
@@ -157,5 +178,4 @@ clusters = int(input("How many clusters would you like to create? ")) #8
 iterations = int(input("How many iterations would you like to run the outlier algorithm for? ")) #30
 percentile = float(input("What percentile (Out of 100) should the outliers be in? "))/100.0 #90
 
-kmedoids = Modified_KMedoids()
-trained_model = kmedoids.k_medoids(array, clusters, iterations, percentile)
+trained_model = Modified_KMedoids.k_medoids(array, clusters, iterations, percentile)
