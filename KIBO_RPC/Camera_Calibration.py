a1a84e82e8ac2365c593c2a18500d59959d3f611
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-04-16 16:44:33 -0400

    Update Camera_Calibration.py

--- KIBO_RPC/Camera_Calibration.py
+++ KIBO_RPC/Camera_Calibration.py
@@ -13,40 +13,51 @@ import cv2.aruco as aruco
 import numpy as np
 
 num_markers_row = 2 #number of rows
-num_markers_column = 1 #numbers of columns
+num_markers_column = 2 #numbers of columns
 
 side_length = 5 #side length of ar tags in cm
 
-dist_to_circle = 3.75 #distance between the x coordinate of ar tag and the x coordinate of circle
+dist_to_circle = 3.75 #distance between the y coordinate of ar tag and the y coordinate of circle
 dist_between_centers = 2 * dist_to_circle
 marker_separation = dist_between_centers - side_length
 
-print(marker_separation)
+dist_to_circle_x = 10
+dist_between_centers_x = 2 * dist_to_circle_x
+marker_separation_x = dist_between_centers_x - side_length
 
-def calibrate_aruco(num_markers_row, num_markers_column, side_length, marker_separation):
+print(marker_separation)
+print(marker_separation_x)
+    
+def calibrate_aruco(num_markers_row, num_markers_column, side_length, marker_separation, marker_separation_x):
     """Calibrates Camera Using ArUco Tags"""
     
     aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
     arucoParams = aruco.DetectorParameters_create()
     
-# =============================================================================
-#     board = aruco.Board_create()
-#     tag_corners = []
-#     for i in range(num_markers_column*num_markers_row):
-#         tag_corner[i][0] = [2.5, 2.5] #top right
-#         tag_corner[i][1] = [2.5, -2.5]
-#         tag_corner[i][2] = [-2.5, -2.5]
-#         tag_corner[i][3] = [-2.5, 2.5]
-# =============================================================================
-        
+    tag_corners = []
+    sep_array_y = [0, marker_separation + side_length, marker_separation + side_length, 0]
+    sep_array_x = [0, 0, marker_separation_x + side_length, marker_separation_x + side_length]
+    for i in range(num_markers_column*num_markers_row):
+        top_left = np.array([sep_array_x[i], side_length + sep_array_y[i], 0], dtype = np.float32)
+        top_right = np.array([side_length + sep_array_x[i], side_length + sep_array_y[i], 0], dtype = np.float32)
+        bottom_right = np.array([side_length + sep_array_x[i], sep_array_y[i], 0], dtype = np.float32)
+        bottom_left = np.array([sep_array_x[i], sep_array_y[i], 0], dtype = np.float32)
+        tag_corners.append(np.array([top_left, top_right, bottom_right, bottom_left]))
     
-    board = aruco.GridBoard_create(num_markers_row, 
-        num_markers_column, 
-        side_length, 
-        marker_separation, 
-        aruco_dict
-    )
-    board.draw((1920, 1080))
+    board_ids = np.array( [[0],[1],[2],[3]], dtype=np.int32)
+    
+    board = aruco.Board_create(tag_corners, aruco_dict, board_ids)
+    
+# =============================================================================
+#     board = aruco.GridBoard_create(
+#         num_markers_column, 
+#         num_markers_row,
+#         side_length, 
+#         marker_separation, 
+#         aruco_dict
+#     )
+#     board.draw((1920, 1080))
+# =============================================================================
     
     # Find the ArUco markers inside target image
     image = cv2.imread(r"D:\Downloads\target_1.png")
@@ -76,7 +87,7 @@ def calibrate_aruco(num_markers_row, num_markers_column, side_length, marker_sep
     """
 
 corners, ids, rejected = calibrate_aruco(num_markers_row, num_markers_column, side_length, marker_separation)
-
+#tag_corners = calibrate_aruco(num_markers_row, num_markers_column, side_length, marker_separation, marker_separation_x)
 
 # =============================================================================
 # #This was commented out because ArUco Grid does not support different x and y separations
