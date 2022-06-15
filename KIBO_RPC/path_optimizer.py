d2d937400d7298d9f792c65c310e29371468dc94
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-06-15 15:55:35 -0400

    yes
    im going to the mall w/ my friends and im gonna upload this in case the mall is boring and i need to code during it

--- KIBO_RPC/path_optimizer.py
+++ KIBO_RPC/path_optimizer.py
@@ -14,7 +14,7 @@ def detectCollision(loc_astrobee): #detects if astrobee's location is within KOZ
     koz_2 = [[9.8673,-9.18813,3.81957],[10.767,-8.288,4.82]]
     koz_3 = [[11.1067,-9.44819,4.87385],[12.0067,-8.89819,5.87385]]
 
-    if (not doesCubeIntersectSphere(koz_1, loc_astrobee) and not doesCubeIntersectSphere(koz_2, loc_astrobee) and not doesCubeIntersectSphere(koz_3, loc_astrobee)):
+    if ((not doesCubeIntersectSphere(koz_1, loc_astrobee)) and (not doesCubeIntersectSphere(koz_2, loc_astrobee)) and (not doesCubeIntersectSphere(koz_3, loc_astrobee))):
         return False #no collision
     else:
         return True #oopsie u done messed up
@@ -35,34 +35,35 @@ def doesCubeIntersectSphere(koz, s):
     elif (s[0] > c2[0]):
         dist_sqr -= (s[0] - c2[0])**2
     
-    if (s[1] < c1[0]):
+    if (s[1] < c1[1]):
         dist_sqr -= (s[1] - c1[1])**2
     elif (s[1] > c2[1]):
         dist_sqr -= (s[1] - c2[1])**2
     
     if (s[2] < c1[2]):
-        dist_sqr -= (s[2] - c2[2])**2
-    elif (s[2] < c2[2]):
+        dist_sqr -= (s[2] - c1[2])**2
+    elif (s[2] > c2[2]):
         dist_sqr -= (s[2] - c2[2])**2
 
+    print(dist_sqr)
     return dist_sqr > 0
 
-def plotKOZ():
+def plotKOZ(ax):
     koz_1 = [[9.8585,-9.45,4.82],[12.0085,-8.5,4.8706]]
     koz_2 = [[9.8673,-9.18813,3.81957],[10.767,-8.288,4.82]]
     koz_3 = [[11.1067,-9.44819,4.87385],[12.0067,-8.89819,5.87385]]
     
-    #plotRectangle(koz_1[0], koz_1[1])
-    plotRectangle(koz_2[0], koz_2[1])
-    #plotRectangle(koz_3[0], koz_3[1])
+    plotRectangle(koz_1[0], koz_1[1], ax)
+    plotRectangle(koz_2[0], koz_2[1], ax)
+    plotRectangle(koz_3[0], koz_3[1], ax)
 
 #i'm stealing so much code from google that it should be considered robbery
-def plotRectangle(c1, c2):
+def plotRectangle(c1, c2, ax):
     all_c = calculateOtherCorners(c1, c2)
     np_c = np.array(all_c)
-    np_c *= 10
     r = [-1, 1]
     X, Y = np.meshgrid(r, r)
+    print(np_c)
     ax.scatter3D(np_c[:, 0], np_c[:, 1], np_c[:, 2])
     verts = [[np_c[0],np_c[1],np_c[2],np_c[3]], #dear anyone with eyes
     [np_c[4],np_c[5],np_c[6],np_c[7]], #i apologize
@@ -74,17 +75,16 @@ def plotRectangle(c1, c2):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    plt.show()
 
 #Behold: The Hudson-wants-to-kill-himself-inator
 def calculateOtherCorners(c1, c2): #Calculates all the other corners of a rectangle formed from two opposite corners
-    all_c = [0,0,0,0,0,0,0,0] #i know what you're thinking and stfu
+    all_c = [0,0,0,0,0,0,0,0]
 
     #floor
     all_c[0] = [c1[0], c1[1], c1[2]]
     all_c[1] = [c1[0], c2[1], c1[2]]
-    all_c[2] = [c2[0], c2[1], c1[0]]
-    all_c[3] = [c2[0], c1[1], c1[0]]
+    all_c[2] = [c2[0], c2[1], c1[2]]
+    all_c[3] = [c2[0], c1[1], c1[2]]
     
     #ceil
     all_c[4] = [c1[0], c1[1], c2[2]]
@@ -94,13 +94,23 @@ def calculateOtherCorners(c1, c2): #Calculates all the other corners of a rectan
     
     return all_c
 
-loc_astrobee = [0, 0, 0]
+def scatterPlotPoints(ax, *args):
+    x = []
+    y = []
+    z = []
+    for point in args:
+        x.append(point[0])
+        y.append(point[1])
+        z.append(point[2])
+    ax.scatter3D(x, y, z, cmap='Greens')
+
+loc_astrobee = [0,0,0]
 point_1 = [10.71, -7.5-0.2725783682, 4.48]
 point_2 = [11.2746-0.0713120911,-9.92284, 5.29881+0.1626665617]
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.scatter3D([point_1[0], point_2[0]], [point_1[1], point_2[1]], [point_1[2], point_2[2]], cmap='Greens')
+axis = fig.add_subplot(111, projection='3d')
+scatterPlotPoints(axis, point_1, point_2, loc_astrobee)
 
-detectCollision(loc_astrobee)
-plotKOZ()
+collide = detectCollision(loc_astrobee)
+plotKOZ(axis)
