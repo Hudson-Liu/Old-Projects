fba29fe500f892de592f8e10b79d59f325050d25
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-06-20 12:47:49 -0400

    Update path_optimizer.py
    I managed to get all the random path generation to work, but unfortunately, it seems that this solution is unfeasible and will not work, due simply to how small of a chance there is of the astrobee properly navigating through the koz through sheer random movement. On the other hand, I have realized that most of the plotting and collision detection code can be repurposed in making a 3D A* algorithm, and that the only stuff I have to overhaul is the stuff I did yesterday which was the random path generation.

--- KIBO_RPC/path_optimizer.py
+++ KIBO_RPC/path_optimizer.py
@@ -2,30 +2,25 @@
 #TODO: Make this code not bad
 
 import math
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
-import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import discriminant_analysis
+import random
+import time
+from tqdm import tqdm, trange
 
-def detectCollision(loc_astrobee): #detects if astrobee's location is within KOZ
+def detectCollision(loc_astrobee, r): #detects if astrobee's location is within KOZ
     koz_1 = [[9.8585,-9.45,4.82],[12.0085,-8.5,4.8706]]
     koz_2 = [[9.8673,-9.18813,3.81957],[10.767,-8.288,4.82]]
     koz_3 = [[11.1067,-9.44819,4.87385],[12.0067,-8.89819,5.87385]]
 
-    if ((not doesCubeIntersectSphere(koz_1, loc_astrobee)) and (not doesCubeIntersectSphere(koz_2, loc_astrobee)) and (not doesCubeIntersectSphere(koz_3, loc_astrobee))):
+    if ((not doesCubeIntersectSphere(koz_1, loc_astrobee, r)) and (not doesCubeIntersectSphere(koz_2, loc_astrobee, r)) and (not doesCubeIntersectSphere(koz_3, loc_astrobee, r))):
         return False #no collision
     else:
-        return True #oopsie u done messed up
+        return True #u done messed up
 
 #True means they intersect, False means that they don't (if you didn't realize that already you probably shouldn't be reading this code)
-def doesCubeIntersectSphere(koz, s):
-    width = 0.25
-    length = 0.25
-    height = 0.25
-    r = math.sqrt(width**2+length**2+height**2) #The robot also rotates so we should treat it as a sphere
-
+def doesCubeIntersectSphere(koz, s, r):
     c1 = koz[0]
     c2 = koz[1]
 
@@ -45,7 +40,6 @@ def doesCubeIntersectSphere(koz, s):
     elif (s[2] > c2[2]):
         dist_sqr -= (s[2] - c2[2])**2
 
-    print(dist_sqr)
     return dist_sqr > 0
 
 def plotKOZ(ax):
@@ -76,6 +70,18 @@ def plotRectangle(c1, c2, ax):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
+#The link below is the poor guy i stole this code from
+#https://stackoverflow.com/questions/40460960/how-to-plot-a-sphere-when-we-are-given-a-central-point-and-a-radius-size
+def plotAstrobee(ax, loc_astrobee, r, n_meridians = 20, n_circles_latitude = None):
+    if n_circles_latitude is None:
+        n_circles_latitude = max(n_meridians/2, 4)
+    u, v = np.mgrid[0:2*np.pi:n_meridians*1j, 0:np.pi:n_circles_latitude*1j]
+    sphere_x = loc_astrobee[0] + r * np.cos(u) * np.sin(v)
+    sphere_y = loc_astrobee[1] + r * np.sin(u) * np.sin(v)
+    sphere_z = loc_astrobee[2] + r * np.cos(v)
+    ax.plot_surface(sphere_x, sphere_y, sphere_z, color="w", edgecolor="r")
+    #ax.plot_surface(sphere_x, sphere_y, sphere_z, color="r", alpha=0.5)
+
 #Behold: The Hudson-wants-to-kill-himself-inator
 def calculateOtherCorners(c1, c2): #Calculates all the other corners of a rectangle formed from two opposite corners
     all_c = [0,0,0,0,0,0,0,0]
@@ -94,6 +100,7 @@ def calculateOtherCorners(c1, c2): #Calculates all the other corners of a rectan
     
     return all_c
 
+#Just a simple plotting function that automatically converts a variable sized list of points into 5
 def scatterPlotPoints(ax, *args):
     x = []
     y = []
@@ -104,13 +111,102 @@ def scatterPlotPoints(ax, *args):
         z.append(point[2])
     ax.scatter3D(x, y, z, cmap='Greens')
 
-loc_astrobee = [0,0,0]
+#Overloading the function to also support lists of points instead of just variable sized parameter lists
+def scatterPlotList(ax, points):
+    x = []
+    y = []
+    z = []
+    for point in points:
+        x.append(point[0])
+        y.append(point[1])
+        z.append(point[2])
+    ax.scatter3D(x, y, z, cmap='Greens')
+    
+def generatePath(point1, point2, goal_reach, reach, r):
+    unsuccessful = True
+    fails = 0
+    #Keeps trying until it's able to create a path under 20000 datapoints
+    while unsuccessful:
+        path = [point1]
+        
+        #Keeps randomly iterating until it reaches the final point
+        #while distanceToPoint(path[i], point2) >= goal_reach:
+        for i in trange(50000):
+            invalid = True
+            while invalid: #Keeps generating possible new waypoints until it finds a valid one
+                new_point = generateRandomPoint(path[i], reach)
+                invalid = detectCollision(new_point, r)
+            path.append(new_point)
+            if distanceToPoint(path[i], point2) <= goal_reach: #this has to run in order for the path to be successful
+                unsuccessful = False
+                break
+            i += 1
+        
+        #Prints this message every time the loop repeats
+        if unsuccessful:
+            fails += 1
+            print("\nFAILURE " + str(fails) + ": Path exceeded iteration number, attempting to generate another path...", flush=True)
+            
+    return path
+
+def distanceToPoint(point1, point2):
+    sum = 0
+    n_dim = 3
+    for i in range(n_dim):
+        sum += (point1[i] - point2[i])**2 #does this for each dimension so you get 3d euclidean distance
+    distance = math.sqrt(sum)
+    return distance
+
+def generateRandomPoint(loc_astrobee, reach): #reach is how far the point is from the original point
+    #get random coordinates using gaussian distribution
+    mu = 0
+    sigma = 10
+    coord = [0.0, 0.0, 0.0]
+    for index in range(len(coord)):
+        coord[index] = random.gauss(mu, sigma)
+    
+    #Find the normalization factor thingy
+    temp = math.sqrt(coord[0]**2+coord[1]**2+coord[2]**2)
+    if temp == 0:
+        print("The normalization divisor somehow equaled zero, shit's boutta go down")
+        normalizer = 1
+    else:
+        normalizer = 1/temp
+    
+    #Normalize all the coordinates
+    for index in range(len(coord)):
+        coord[index] = coord[index] * normalizer * reach
+    
+    #Add current positional value
+    for index in range(len(loc_astrobee)):
+        coord[index] += loc_astrobee[index]
+        
+    return coord
+
+loc_astrobee = [12.0067,-8.89819,5.87385]
 point_1 = [10.71, -7.5-0.2725783682, 4.48]
 point_2 = [11.2746-0.0713120911,-9.92284, 5.29881+0.1626665617]
 
+#astrobee bounding box definition
+width = 0.25
+length = 0.25
+height = 0.25
+r = math.sqrt(width**2+length**2+height**2) #The robot also rotates so we should treat it as a sphere
+
+collide = detectCollision(loc_astrobee, r)
+
 fig = plt.figure()
 axis = fig.add_subplot(111, projection='3d')
-scatterPlotPoints(axis, point_1, point_2, loc_astrobee)
 
-collide = detectCollision(loc_astrobee)
+#visualizing everything
+scatterPlotPoints(axis, point_1, point_2, loc_astrobee)
 plotKOZ(axis)
+plotAstrobee(axis, loc_astrobee, r)
+
+#TODO: REMOVE THIS, purely for debugging purposes
+#temp = []
+#for i in tqdm(range(1000), desc="generating random points"):
+#    temp.append(generateRandomPoint(loc_astrobee, 5))#5 is extreme, the real value is ~0.01, this is just for demo purposes
+#scatterPlotList(axis, temp)
+path = generatePath(point_1, point_2, 0.2, 0.01, r)
+scatterPlotList(axis, path)
